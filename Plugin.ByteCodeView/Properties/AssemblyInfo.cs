b4da2f01ab96c83702d8f8d6c68da3e6460c11b7
DKorablin2 <danila.korablin@unity3d.com>
2025-02-23 23:55:25 +0300

    Unified plugins deployment

--- Plugin.ByteCodeView/Properties/AssemblyInfo.cs
+++ Plugin.ByteCodeView/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("229ecb2b-b6c7-46e3-b8b6-9bac932f0679")]
 [assembly: System.CLSCompliant(false)]
 
@@ -9,16 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://dkorablin.ru/project/Default.aspx?File=112")]
 #else
 
-[assembly: AssemblyTitle("Plugin.ByteCodeView")]
 [assembly: AssemblyDescription("Java ByteCode (JVM) viewer plugin")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.ByteCodeView")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2016-2024")]
+[assembly: AssemblyCopyright("Copyright © Danila Korablin 2016-2025")]
 #endif
 
 /*
